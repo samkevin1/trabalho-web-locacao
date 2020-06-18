d9a84532a3df95c3819a5439fa284b4ff8d3ce8f
André Rebonato <andrerebonato@users.noreply.github.com>
2020-06-18 11:34:08 -0300

    Refatorado

---
+++ app/src/components/displayLoading/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export default function() {
+    return(
+        <Fragment>
+            <h3 class="text-center">Carregando informações, aguarde...</h3>
+        </Fragment>
+    )
+}
