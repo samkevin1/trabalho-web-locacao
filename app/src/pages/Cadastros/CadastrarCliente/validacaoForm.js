514d2654a5471531841d888bc90f4261a3ccd0f5
André Rebonato <andrerebonato@users.noreply.github.com>
2020-06-18 11:33:32 -0300

    Refatorado validacao

---
+++ app/src/pages/Cadastros/CadastrarCliente/validacaoForm.js
@@ -0,0 +1,20 @@
+import * as Yup from 'yup';
+
+export default Yup.object().shape({
+    nome: Yup.string()
+        .required('Preencha este campo...')
+        .max(45, 'Esse campo tem no máximo 45 caractéres...'),
+    sobrenome: Yup.string()
+        .required('Preencha este campo...')
+        .max(45, 'Esse campo tem no máximo 45 caractéres...'),
+    cpf: Yup.string()
+        .required('Preencha este campo...')
+        .max(14, 'Esse campo tem no máximo 14 caractéres...')
+        .min('Esse campo tem no mínimo 11 caratéres...'),
+    telefone: Yup.string()
+        .notRequired()
+        .max(45, 'Esse campo tem no máximo 45 caractéres...'),
+    cnh: Yup.string()
+        .required('Preencha este campo...')
+        .max(45, 'Esse campo tem no máximo 45 caractéres...')
+});
